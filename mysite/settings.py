f29f674edfba640c67b771be0f0c4ec33dee84d7
cc8en <46859219+cc8en@users.noreply.github.com>
2019-02-18 09:28:43 -0500

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'bootstrap4',
 ]
 
@@ -129,5 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join('mysite', 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join('mysite', 'static'),
+]
